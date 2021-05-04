a920d9ef5b6353d1f240e24b90203767ed70211b
Oluwafemi <phemmylintry@gmail.com>
2021-05-04 12:25:04 +0100

    budget task assignment

--- Classes and Objects/classes.py
+++ Classes and Objects/classes.py
@@ -1,30 +1,41 @@
 class Category:
     
-    #constructor
+
     def __init__(self, category, amount): 
         self.category = category
         self.amount = amount
     
-    #methods
+
     def deposit(self, amount):
         self.amount += amount
         return "You have successfully deposited {} in {} category".format(amount, self.category)
 
+    
     def budget_balance(self):
         return "This is the current balance: {}".format(self.amount)
 
+    
     def check_balance(self, amount):
-        #this should return a True or False, it checks if amount is less or greater than self.amount
-        pass
+        if self.amount > amount:
+            return True
+        else:
+            return False
 
+    
     def withdraw(self, amount):
-        #reverse of deposit
-        pass
+        self.amount -= amount
+        return "You have successfully deposited {} in {} category".format(amount, self.category)
 
 
     def transfer(self, amount, catergory):
-        #transfer between two instantiated categories
-        pass
+        
+        if not self.check_balance(amount):
+            return "Transfer not successful"
+        
+        self.amount -= amount
+        catergory.amount += amount
+        return "You have transferred {} to {} category".format(amount, catergory.category)
+
 
 
 food_category = Category("Food", 500)
@@ -33,3 +44,6 @@ car_category = Category("Car Expenses", 600)
 
 print(food_category.deposit(250))
 print(food_category.budget_balance())
+print(food_category.transfer(100, clothing_category))
+print(food_category.budget_balance())
+print(clothing_category.budget_balance())
